d402b6956ceb5acb6fb626ca80bc78f7acc7d4c5
Mad-FOX <sarb.rb@gmail.com>
2018-02-13 04:25:37 +0600

    File prepation for PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+#from setuptools import setup
+from distutils.core import setup
 
-setup(name='bijoy2unicode',
+setup(
+      name='bijoy2unicode',
       version='0.1.0',
       description='Convert text/file written in Bijoy to Unicode',
       url='http://github.com/mad-fox/bijoy2unicode',
+      download_url = 'http://github.com/mad-fox/bijoy2unicode/archive/0.1.0.tar.gz',
       author='Rajib Biswas',
       author_email='sarb.rb@gmail.com',
       license='MIT',
       packages=['bijoy2unicode'],
-      zip_safe=False)
+      zip_safe=False,
+      keywords = ['bengali', 'bangla', 'nlp', 'bijoy', 'avro', 'unicode'],
+      classifiers = []
+)
