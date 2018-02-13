cc2a39c664cb0140f1b22f54630c312e98741b0d
Mad-FOX <sarb.rb@gmail.com>
2018-02-14 00:20:55 +0600

    Version 0.1.1 Fixed: Unicode to Bijoy conversion issue (#1).

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ The package is also available via pip.
 
 ## Version
 
->  0.1.0
+>  0.1.1
+
+## License
+MIT
 
 ## Installation
 
@@ -20,9 +23,12 @@ The package is also available via pip.
     bijoy_text = 'Dfq cv‡k av‡bi kx‡l †ewóZ cvwb‡Z fvmgvb RvZxq dzj kvcjv| Zvi gv_vq cvUMv‡Qi ci¯úi mshy³ wZbwU cvZv Ges Dfh cv‡k `ywU K‡i ZviKv|'
     
     print(bijoy_text)
+    
     toPrint=test.convertBijoyToUnicode(bijoy_text)
     print(toPrint)
-    toPrint=test.convertUnicodeToBijoy(bijoy_text)
+    # উভয় পাশে ধানের শীষে বেষ্টিত পানিতে ভাসমান জাতীয় ফুল শাপলা। তার মাথায় পাটগাছের পরস্পর সংযুক্ত তিনটি পাতা এবং উভয পাশে দুটি করে তারকা।
+
+    toPrint=test.convertUnicodeToBijoy(toPrint)
     print(toPrint)
 
 
@@ -31,3 +37,5 @@ The package is also available via pip.
 https://github.com/bahar/BijoyToUnicode
 
 https://github.com/sh-sabbir/UnicodeToBijoy
+
+https://blog.jetbrains.com/pycharm/2017/05/how-to-publish-your-package-on-pypi/
--- bijoy2unicode/converter.py
+++ bijoy2unicode/converter.py
@@ -410,6 +410,68 @@ class Unicode:
             i += 1
         return str
 
+    def reArranceUnicodeTextForASCI(self, str):
+        
+        cY = 0
+        i = 0
+        
+        #for ($i = 0; $i < util.mb_strlen(str); ++$i) 
+        while i<util.mb_strlen(str):
+
+            if(i<util.mb_strlen(str) and self.IsBanglaPreKar(util.mbCharAt(str,i))):
+                j=1
+                while self.IsBanglaBanjonborno(util.mbCharAt(str,i-j)):
+                    if (i-j)<0:
+                        break
+                    if (i-j)<=cY:
+                        break
+                    if self.IsBanglaHalant(util.mbCharAt(str,i-j-1)):
+                        j+=2
+                    else:
+                        break
+
+                R = util.subString(str,0, i-j)
+                R += util.mbCharAt(str,i)
+                R += util.subString(str,i-j, i)
+                R += util.subString(str,i+1, util.mb_strlen(str))
+                
+                str = R
+                
+                cY= i+1
+                continue
+            
+            
+            if i<(util.mb_strlen(str)-1) and self.IsBanglaHalant(util.mbCharAt(str,i)) and util.mbCharAt(str,i-1)=='র' and not self.IsBanglaHalant(util.mbCharAt(str,i-2)):
+                j=1
+                aZ=0
+
+                while True:
+                    if self.IsBanglaBanjonborno(util.mbCharAt(str,i+j)) and self.IsBanglaHalant(util.mbCharAt(str,i+j+1)):
+                        j+=2
+
+                    elif self.IsBanglaBanjonborno(util.mbCharAt(str,i+j)) and self.IsBanglaPreKar(util.mbCharAt(str,i+j+1)):
+                        aZ=1
+                        break
+                    
+                    else:
+                        break
+
+                R  = util.subString(str,0, i-1)
+                R += util.subString(str, i+j+1, i+j+aZ+1)
+                R += util.subString(str, i+1, i+j+1)
+                R += util.mbCharAt(str, i-1)
+                R += util.mbCharAt(str, i)
+                R += util.subString(str, i+j+aZ+1, util.mb_strlen(str))
+                
+                str = R
+                
+                i+=(j+aZ)
+                cY=i+1
+                continue
+
+            i += 1
+        return str
+
     # main conversion def
     def convertBijoyToUnicode(self, srcString):
         if not srcString:
@@ -426,8 +488,97 @@ class Unicode:
         if not srcString:
             return srcString
 
-        inv_conversionMap = {v: k for k, v in conversionMap.items()}
-        srcString = util.doCharMap(srcString, inv_conversionMap)
+        main_char = {
+            "।"   :   "|", 	"‘"   :   "Ô", 	"’"   :   "Õ", 
+            "“"   :   "Ò", 	"”"   :   "Ó", 	"্র্য"   :   "ª¨", 
+            "ম্প্র"   :   "¤cÖ", 	"র‌্য"   :   "i¨", 	"ক্ষ্ম"   :   "²", 
+            "ক্ক"   :   "°", 	"ক্ট"   :   "±", 	"ক্ত"   :   "³", 
+            "ক্ব"   :   "K¡", 	"স্ক্র"   :   "¯Œ", 	"ক্র"   :   "µ", 
+            "ক্ল"   :   "K¬", 	"ক্ষ"   :   "¶", 	"ক্স"   :   "·", 
+            "গু"   :   "¸", 	"গ্ধ"   :   "»", 	"গ্ন"   :   "Mœ", 
+            "গ্ম"   :   "M¥", 	"গ্ল"   :   "M­", 	"গ্রু"   :   "Mªy", 
+            "ঙ্ক"   :   "¼", 	"ঙ্ক্ষ"   :   "•¶", 	"ঙ্খ"   :   "•L", 
+            "ঙ্গ"   :   "½", 	"ঙ্ঘ"   :   "•N", 	"চ্ছ্ব"   :   "”Q¡", 
+            "চ্চ"   :   "”P", 	"চ্ছ"   :   "”Q", 	"চ্ঞ"   :   "”T", 
+            "জ্জ্ব"   :   "¾¡", 	"জ্জ"   :   "¾", 	"জ্ঝ"   :   "À", 
+            "জ্ঞ"   :   "Á", 	"জ্ব"   :   "R¡", 	"ঞ্চ"   :   "Â", 
+            "ঞ্ছ"   :   "Ã", 	"ঞ্জ"   :   "Ä", 	"ঞ্ঝ"   :   "Å", 
+            "ট্ট"   :   "Æ", 	"ট্ব"   :   "U¡", 	"ট্ম"   :   "U¥", 
+            "ড্ড"   :   "Ç", 	"ণ্ট"   :   "È", 	"ণ্ঠ"   :   "É", 
+            "ন্স"   :   "Ý", 	"ণ্ড"   :   "Ê", 	"ন্তু"   :   "š‘", 
+            "ণ্ব"   :   "Y^", 	"ত্ত্ব"   :   "Ë¡", 	"ত্ত"   :   "Ë", 
+            "ত্থ"   :   "Ì", 	"ত্ন"   :   "Zœ", 	"ত্ম"   :   "Z¥", 
+            "ন্ত্ব"   :   "š—¡", 	"ত্ব"   :   "Z¡", 	"থ্ব"   :   "_¡", 
+            "দ্গ"   :   "˜M", 	"দ্ঘ"   :   "˜N", 	"দ্দ"   :   "Ï", 
+            "দ্ধ"   :   "×", 	"দ্ব"   :   "˜¡", 	"দ্ব"   :   "Ø", 
+            "দ্ভ"   :   "™¢", 	"দ্ম"   :   "Ù", 	"দ্রু"   :   "`ª“", 
+            "ধ্ব"   :   "aŸ", 	"ধ্ম"   :   "a¥", 	"ন্ট"   :   "›U", 
+            "ন্ঠ"   :   "Ú", 	"ন্ড"   :   "Û", 	"ন্ত্র"   :   "š¿", 
+            "ন্ত"   :   "š—", 	"স্ত্র"   :   "¯¿", 	"ত্র"   :   "Î", 
+            "ন্থ"   :   "š’", 	"ন্দ"   :   "›`", 	"ন্দ্ব"   :   "›Ø", 
+            "ন্ধ"   :   "Ü", 	"ন্ন"   :   "bœ", 	"ন্ব"   :   "š^", 
+            "ন্ম"   :   "b¥", 	"প্ট"   :   "Þ", 	"প্ত"   :   "ß", 
+            "প্ন"   :   "cœ", 	"প্প"   :   "à", 	"প্ল"   :   "c­", 
+            "প্স"   :   "á", 	"ফ্ল"   :   "d¬", 	"ব্জ"   :   "â", 
+            "ব্দ"   :   "ã", 	"ব্ধ"   :   "ä", 	"ব্ব"   :   "eŸ", 
+            "ব্ল"   :   "e­", 	"ভ্র"   :   "å", 	"ম্ন"   :   "gœ", 
+            "ম্প"   :   "¤ú", 	"ম্ফ"   :   "ç", 	"ম্ব"   :   "¤^", 
+            "ম্ভ"   :   "¤¢", 	"ম্ভ্র"   :   "¤£", 	"ম্ম"   :   "¤§", 
+            "ম্ল"   :   "¤­", 	"্র"   :   "ª", 	"রু"   :   "i“", 
+            "রূ"   :   "iƒ", 	"ল্ক"   :   "é", 	"ল্গ"   :   "ê", 
+            "ল্ট"   :   "ë", 	"ল্ড"   :   "ì", 	"ল্প"   :   "í", 
+            "ল্ফ"   :   "î", 	"ল্ব"   :   "j¦", 	"ল্ম"   :   "j¥", 
+            "ল্ল"   :   "j­", 	"শু"   :   "ï", 	"শ্চ"   :   "ð", 
+            "শ্ন"   :   "kœ", 	"শ্ব"   :   "k¦", 	"শ্ম"   :   "k¥", 
+            "শ্ল"   :   "k­", 	"ষ্ক"   :   "®‹", 	"ষ্ক্র"   :   "®Œ", 
+            "ষ্ট"   :   "ó", 	"ষ্ঠ"   :   "ô", 	"ষ্ণ"   :   "ò", 
+            "ষ্প"   :   "®ú", 	"ষ্ফ"   :   "õ", 	"ষ্ম"   :   "®§", 
+            "স্ক"   :   "¯‹", 	"স্ট"   :   "÷", 	"স্খ"   :   "ö", 
+            "স্ত"   :   "¯—", 	"স্তু"   :   "¯‘", 	"স্থ"   :   "¯’", 
+            "স্ন"   :   "mœ", 	"স্প"   :   "¯ú", 	"স্ফ"   :   "ù", 
+            "স্ব"   :   "¯^", 	"স্ম"   :   "¯§", 	"স্ল"   :   "¯­", 
+            "হু"   :   "û", 	"হ্ণ"   :   "nè", 	"হ্ব"   :   "nŸ", 
+            "হ্ন"   :   "ý", 	"হ্ম"   :   "þ", 	"হ্ল"   :   "n¬", 
+            "হৃ"   :   "ü", 	"র্"   :   "©", 	"্র"   :   "«", 
+            "্য"   :   "¨", 	"্"   :   "&", 	"আ"   :   "Av", 
+            "অ"   :   "A", 	"ই"   :   "B", 	"ঈ"   :   "C", 
+            "উ"   :   "D", 	"ঊ"   :   "E", 	"ঋ"   :   "F", 
+            "এ"   :   "G", 	"ঐ"   :   "H", 	"ও"   :   "I", 
+            "ঔ"   :   "J", 	"ক"   :   "K", 	"খ"   :   "L", 
+            "গ"   :   "M", 	"ঘ"   :   "N", 	"ঙ"   :   "O", 
+            "চ"   :   "P", 	"ছ"   :   "Q", 	"জ"   :   "R", 
+            "ঝ"   :   "S", 	"ঞ"   :   "T", 	"ট"   :   "U", 
+            "ঠ"   :   "V", 	"ড"   :   "W", 	"ঢ"   :   "X", 
+            "ণ"   :   "Y", 	"ত"   :   "Z", 	"থ"   :   "_", 
+            "দ"   :   "`", 	"ধ"   :   "a", 	"ন"   :   "b", 
+            "প"   :   "c", 	"ফ"   :   "d", 	"ব"   :   "e", 
+            "ভ"   :   "f", 	"ম"   :   "g", 	"য"   :   "h", 
+            "র"   :   "i", 	"ল"   :   "j", 	"শ"   :   "k", 
+            "ষ"   :   "l", 	"স"   :   "m", 	"হ"   :   "n", 
+            "ড়"   :   "o", 	"ঢ়"   :   "p", 	"য়"   :   "q", 
+            "ৎ"   :   "r", 	"০"   :   "0", 	"১"   :   "1", 
+            "২"   :   "2", 	"৩"   :   "3", 	"৪"   :   "4", 
+            "৫"   :   "5", 	"৬"   :   "6", 	"৭"   :   "7", 
+            "৮"   :   "8", 	"৯"   :   "9", 	"া"   :   "v", 
+            "ি"   :   "w", 	"ী"   :   "x", 	"ু"   :   "y", 
+            "ূ"   :   "~", 	"ৃ"   :   "…", 	"ে"   :   "‡", 
+            "ৈ"   :   "‰", 	"ৗ"   :   "Š", 	"ং"   :   "s", 
+            "ঃ"   :   "t", 	"ঁ"   :   "u"
+        }
+
+        pattern = 'ো'
+        replacement = 'ো'
+        srcString = util.preg_replace(pattern, replacement, srcString)
+	
+        pattern = 'ৌ'
+        replacement = 'ৌ'
+        srcString = util.preg_replace(pattern, replacement, srcString)
+
+        # make correction
+        srcString = self.reArranceUnicodeTextForASCI(srcString)
+
+        #inv_conversionMap = {v: k for k, v in conversionMap.items()}
+        srcString = util.doCharMap(srcString, main_char)
         
         return srcString
 
