5b6f52fc4d76c186cb6c7f4d613ef1c3ac7a1974
AsadDUET <asadouzzaman@gmail.com>
2020-11-21 06:52:13 +0600

    fixed issue with two letters "ড়" and "য়"

--- converter.py
+++ converter.py
@@ -295,25 +295,25 @@ class Unicode:
 		#global proConversionMap
 
 		#for (i = 0; i < mb_strlen(str); ++i)
- 
+
 		i = 0
 		while i < util.mb_strlen(str):
 			#  Change refs
-			
+
 			if (i < (util.mb_strlen(str) - 1) and util.mbCharAt(str, i) == 'র' and self.IsBanglaHalant(util.mbCharAt(str, i + 1)) and not self.IsBanglaHalant(util.mbCharAt(str, i - 1))):
 				j = 1
 				while (True):
 					if (i - j < 0):
 						break
-					
-					if (self.IsBanglaBanjonborno(util.mbCharAt(str, i - j)) and self.IsBanglaHalant(util.mbCharAt(str, i - j - 1))): 
+
+					if (self.IsBanglaBanjonborno(util.mbCharAt(str, i - j)) and self.IsBanglaHalant(util.mbCharAt(str, i - j - 1))):
 						j += 2
 					elif (j == 1 and self.IsBanglaKar(util.mbCharAt(str, i - j))):
 						j += 1
 					else:
 						break
-					
-				
+
+
 				temp = util.subString(str, 0, i - j)
 				temp += util.mbCharAt(str, i)
 				temp += util.mbCharAt(str, i + 1)
@@ -327,7 +327,7 @@ class Unicode:
 
 		#str = util.doCharMap(str, proConversionMap)
 
-		#for (i = 0 i < mb_strlen(str) ++i) 
+		#for (i = 0 i < mb_strlen(str) ++i)
 		i=0
 		while i<util.mb_strlen(str):
 			if (i < util.mb_strlen(str) - 1 and util.mbCharAt(str, i) == 'র' and self.IsBanglaHalant(util.mbCharAt(str, i + 1)) and not self.IsBanglaHalant(util.mbCharAt(str, i - 1)) and self.IsBanglaHalant(util.mbCharAt(str, i + 2))):
@@ -335,15 +335,15 @@ class Unicode:
 				while (True):
 					if (i - j < 0):
 						break
-					
+
 					if (self.IsBanglaBanjonborno(util.mbCharAt(str, i - j)) and self.IsBanglaHalant(util.mbCharAt(str, i - j - 1))):
 						j += 2
 					elif (j == 1 and self.IsBanglaKar(util.mbCharAt(str, i - j))):
 						j += 1
 					else:
 						break
-					
-				
+
+
 				temp = util.subString(str, 0, i - j)
 				temp += util.mbCharAt(str, i)
 				temp += util.mbCharAt(str, i + 1)
@@ -352,7 +352,7 @@ class Unicode:
 				str = temp
 				i += 1
 				continue
-			
+
 
 			#  for 'Vowel + HALANT + Consonant' it should be 'HALANT + Consonant + Vowel'
 			if (i > 0 and util.mbCharAt(str, i) == '\u09CD' and (self.IsBanglaKar(util.mbCharAt(str, i - 1)) or self.IsBanglaNukta(util.mbCharAt(str, i - 1))) and i < util.mb_strlen(str) - 1):
@@ -362,7 +362,7 @@ class Unicode:
 				temp += util.mbCharAt(str, i - 1)
 				temp += util.subString(str, i + 2, util.mb_strlen(str))
 				str = temp
-			
+
 
 			#  for 'RA (\u09B0) + HALANT + Vowel' it should be 'Vowel + RA (\u09B0) + HALANT'
 			if (i > 0 and i < util.mb_strlen(str) - 1 and util.mbCharAt(str, i) == '\u09CD' and util.mbCharAt(str, i - 1) == '\u09B0' and util.mbCharAt(str, i - 2) != '\u09CD' and self.IsBanglaKar(util.mbCharAt(str, i + 1))):
@@ -371,11 +371,11 @@ class Unicode:
 				temp += util.mbCharAt(str, i - 1)
 				temp += util.mbCharAt(str, i)
 				temp += util.subString(str, i + 2, util.mb_strlen(str))
-				str = temp			  
+				str = temp
 
 			#  Change pre-kar to post format suitable for unicode
 			if (i < util.mb_strlen(str) - 1 and self.IsBanglaPreKar(util.mbCharAt(str, i)) and self.IsSpace(util.mbCharAt(str, i + 1)) == False):
-				
+
 				temp = util.subString(str, 0, i)
 
 				j = 1
@@ -383,8 +383,8 @@ class Unicode:
 					if ((i + j) < util.mb_strlen(str) and self.IsBanglaHalant(util.mbCharAt(str, i + j + 1))):
 						j += 2
 					else:
-						break					 
-				
+						break
+
 				temp += util.subString(str, i + 1, i + j + 1)
 
 				l = 0
@@ -396,11 +396,11 @@ class Unicode:
 					l = 1
 				else:
 					temp += util.mbCharAt(str, i)
-				
+
 				temp += util.subString(str, i + j + l + 1, util.mb_strlen(str))
 				str = temp
 				i += j
-			
+
 			#  nukta should be placed after kars
 			if (i < util.mb_strlen(str) - 1 and self.IsBanglaNukta(util.mbCharAt(str, i)) and self.IsBanglaPostKar(util.mbCharAt(str, i + 1))):
 				temp = util.subString(str, 0, i)
@@ -413,11 +413,11 @@ class Unicode:
 		return str
 
 	def reArranceUnicodeTextForASCI(self, str):
-		
+
 		cY = 0
 		i = 0
-		
-		#for ($i = 0; $i < util.mb_strlen(str); ++$i) 
+
+		#for ($i = 0; $i < util.mb_strlen(str); ++$i)
 		while i<util.mb_strlen(str):
 
 			if(i<util.mb_strlen(str) and self.IsBanglaPreKar(util.mbCharAt(str,i))):
@@ -436,13 +436,13 @@ class Unicode:
 				R += util.mbCharAt(str,i)
 				R += util.subString(str,i-j, i)
 				R += util.subString(str,i+1, util.mb_strlen(str))
-				
+
 				str = R
-				
+
 				cY= i+1
 				continue
-			
-			
+
+
 			if i<(util.mb_strlen(str)-1) and self.IsBanglaHalant(util.mbCharAt(str,i)) and util.mbCharAt(str,i-1)=='র' and not self.IsBanglaHalant(util.mbCharAt(str,i-2)):
 				j=1
 				aZ=0
@@ -454,7 +454,7 @@ class Unicode:
 					elif self.IsBanglaBanjonborno(util.mbCharAt(str,i+j)) and self.IsBanglaPreKar(util.mbCharAt(str,i+j+1)):
 						aZ=1
 						break
-					
+
 					else:
 						break
 
@@ -464,9 +464,9 @@ class Unicode:
 				R += util.mbCharAt(str, i-1)
 				R += util.mbCharAt(str, i)
 				R += util.subString(str, i+j+aZ+1, util.mb_strlen(str))
-				
+
 				str = R
-				
+
 				i+=(j+aZ)
 				cY=i+1
 				continue
@@ -478,93 +478,101 @@ class Unicode:
 	def convertUnicodeToBijoy(self, srcString):
 		if not srcString:
 			return srcString
-		
+
 		srcString = "কর্ড" + srcString
-		
+
 		main_char = {
-			"।"	  :	  "|",	"‘"	  :	  "Ô",	"’"	  :	  "Õ", 
-			"“"	  :	  "Ò",	"”"	  :	  "Ó",	"্র্য"	 :	 "ª¨", 
-			"ম্প্র"	  :	  "¤cÖ",	"র‌্য"	 :	 "i¨",	"ক্ষ্ম"	  :	  "²", 
-			"ক্ক"	:	"°",	"ক্ট"	:	"±",	"ক্ত"	:	"³", 
-			"ক্ব"	:	"K¡",	"স্ক্র"	  :	  "¯Œ",		"ক্র"	:	"µ", 
-			"ক্ল"	:	"K¬",	"ক্ষ"	:	"¶",	"ক্স"	:	"·", 
-			"গু"   :   "¸",		"গ্ধ"	:	"»",	"গ্ন"	:	"Mœ", 
-			"গ্ম"	:	"M¥",	"গ্ল"	:	"M­",	"গ্রু"	 :	 "Mªy", 
-			"ঙ্ক"	:	"¼",	"ঙ্ক্ষ"	  :	  "•¶",		"ঙ্খ"	:	"•L", 
-			"ঙ্গ"	:	"½",	"ঙ্ঘ"	:	"•N",	"চ্ছ্ব"	  :	  "”Q¡", 
-			"চ্চ"	:	"”P",	"চ্ছ"	:	"”Q",	"চ্ঞ"	:	"”T", 
-			"জ্জ্ব"	  :	  "¾¡",		"জ্জ"	:	"¾",	"জ্ঝ"	:	"À", 
-			"জ্ঞ"	:	"Á",	"জ্ব"	:	"R¡",	"ঞ্চ"	:	"Â", 
-			"ঞ্ছ"	:	"Ã",	"ঞ্জ"	:	"Ä",	"ঞ্ঝ"	:	"Å", 
-			"ট্ট"	:	"Æ",	"ট্ব"	:	"U¡",	"ট্ম"	:	"U¥", 
-			"ড্ড"	:	"Ç",	"ণ্ট"	:	"È",	"ণ্ঠ"	:	"É", 
-			"ন্স"	:	"Ý",	"ণ্ড"	:	"Ê",	"ন্তু"	 :	 "š‘", 
-			"ণ্ব"	:	"Y^",	"ত্ত্ব"	  :	  "Ë¡",		"ত্ত"	:	"Ë", 
-			"ত্থ"	:	"Ì",	"ত্ন"	:	"Zœ",	"ত্ম"	:	"Z¥", 
-			"ন্ত্ব"	  :	  "š—¡",	"ত্ব"	:	"Z¡",	"থ্ব"	:	"_¡", 
-			"দ্গ"	:	"˜M",	"দ্ঘ"	:	"˜N",	"দ্দ"	:	"Ï", 
-			"দ্ধ"	:	"×",	"দ্ব"	:	"˜¡",	"দ্ব"	:	"Ø", 
-			"দ্ভ"	:	"™¢",	"দ্ম"	:	"Ù",	"দ্রু"	 :	 "`ª“", 
-			"ধ্ব"	:	"aŸ",	"ধ্ম"	:	"a¥",	"ন্ট"	:	"›U", 
-			"ন্ঠ"	:	"Ú",	"ন্ড"	:	"Û",	"ন্ত্র"	  :	  "š¿", 
-			"ন্ত"	:	"š—",	"স্ত্র"	  :	  "¯¿",		"ত্র"	:	"Î", 
-			"ন্থ"	:	"š’",	"ন্দ"	:	"›`",	"ন্দ্ব"	  :	  "›Ø", 
-			"ন্ধ"	:	"Ü",	"ন্ন"	:	"bœ",	"ন্ব"	:	"š^", 
-			"ন্ম"	:	"b¥",	"প্ট"	:	"Þ",	"প্ত"	:	"ß", 
-			"প্ন"	:	"cœ",	"প্প"	:	"à",	"প্ল"	:	"cø", 
-			"প্স"	:	"á",	"ফ্ল"	:	"d¬",	"ব্জ"	:	"â", 
-			"ব্দ"	:	"ã",	"ব্ধ"	:	"ä",	"ব্ব"	:	"eŸ", 
-			"ব্ল"	:	"e­",	"ভ্র"	:	"å",	"ম্ন"	:	"gœ", 
-			"ম্প"	:	"¤ú",	"ম্ফ"	:	"ç",	"ম্ব"	:	"¤^", 
-			"ম্ভ"	:	"¤¢",	"ম্ভ্র"	  :	  "¤£",		"ম্ম"	:	"¤§", 
-			"ম্ল"	:	"¤­",	"্র"   :   "ª",		"রু"   :   "i“", 
-			"রূ"   :   "iƒ",	"ল্ক"	:	"é",	"ল্গ"	:	"ê", 
-			"ল্ট"	:	"ë",	"ল্ড"	:	"ì",	"ল্প"	:	"í", 
-			"ল্ফ"	:	"î",	"ল্ব"	:	"j¦",	"ল্ম"	:	"j¥", 
-			"ল্ল"	:	"jø",	"শু"   :   "ï",		"শ্চ"	:	"ð", 
-			"শ্ন"	:	"kœ",	"শ্ব"	:	"k¦",	"শ্ম"	:	"k¥", 
-			"শ্ল"	:	"k­",	"ষ্ক"	:	"®‹",	"ষ্ক্র"	  :	  "®Œ", 
-			"ষ্ট"	:	"ó",	"ষ্ঠ"	:	"ô",	"ষ্ণ"	:	"ò", 
-			"ষ্প"	:	"®ú",	"ষ্ফ"	:	"õ",	"ষ্ম"	:	"®§", 
-			"স্ক"	:	"¯‹",	"স্ট"	:	"÷",	"স্খ"	:	"ö", 
-			"স্ত"	:	"¯Í",	"স্তু"	 :	 "¯‘",	"স্থ"	:	"¯’", 
-			"স্ন"	:	"mœ",	"স্প"	:	"¯ú",	"স্ফ"	:	"ù", 
-			"স্ব"	:	"¯^",	"স্ম"	:	"¯§",	"স্ল"	:	"¯­", 
-			"হু"   :   "û",		"হ্ণ"	:	"nè",	"হ্ব"	:	"nŸ", 
-			"হ্ন"	:	"ý",	"হ্ম"	:	"þ",	"হ্ল"	:	"n¬", 
-			"হৃ"   :   "ü",		"র্"   :   "©",		"্র"   :   "«", 
-			"্য"   :   "¨",		"্"	  :	  "&",	"আ"	  :	  "Av", 
-			"অ"	  :	  "A",	"ই"	  :	  "B",	"ঈ"	  :	  "C", 
-			"উ"	  :	  "D",	"ঊ"	  :	  "E",	"ঋ"	  :	  "F", 
-			"এ"	  :	  "G",	"ঐ"	  :	  "H",	"ও"	  :	  "I", 
-			"ঔ"	  :	  "J",	"ক"	  :	  "K",	"খ"	  :	  "L", 
-			"গ"	  :	  "M",	"ঘ"	  :	  "N",	"ঙ"	  :	  "O", 
-			"চ"	  :	  "P",	"ছ"	  :	  "Q",	"জ"	  :	  "R", 
-			"ঝ"	  :	  "S",	"ঞ"	  :	  "T",	"ট"	  :	  "U", 
-			"ঠ"	  :	  "V",	"ড"	  :	  "W",	"ঢ"	  :	  "X", 
-			"ণ"	  :	  "Y",	"ত"	  :	  "Z",	"থ"	  :	  "_", 
-			"দ"	  :	  "`",	"ধ"	  :	  "a",	"ন"	  :	  "b", 
-			"প"	  :	  "c",	"ফ"	  :	  "d",	"ব"	  :	  "e", 
-			"ভ"	  :	  "f",	"ম"	  :	  "g",	"য"	  :	  "h", 
-			"র"	  :	  "i",	"ল"	  :	  "j",	"শ"	  :	  "k", 
-			"ষ"	  :	  "l",	"স"	  :	  "m",	"হ"	  :	  "n", 
-			"ড়"	  :	  "o",	"ঢ়"	  :	  "p",	"য়"	  :	  "q", 
-			"ৎ"	  :	  "r",	"০"	  :	  "0",	"১"	  :	  "1", 
-			"২"	  :	  "2",	"৩"	  :	  "3",	"৪"	  :	  "4", 
-			"৫"	  :	  "5",	"৬"	  :	  "6",	"৭"	  :	  "7", 
-			"৮"	  :	  "8",	"৯"	  :	  "9",	"া"	  :	  "v", 
-			"ি"	  :	  "w",	"ী"	  :	  "x",	"ু"	  :	  "y", 
-			"ূ"	  :	  "~",	"ৃ"	  :	  "…",	"ে"	  :	  "‡", 
-			"ৈ"	  :	  "‰",	"ৗ"	  :	  "Š",	"ং"	  :	  "s", 
+			"।"	  :	  "|",	"‘"	  :	  "Ô",	"’"	  :	  "Õ",
+			"“"	  :	  "Ò",	"”"	  :	  "Ó",	"্র্য"	 :	 "ª¨",
+			"ম্প্র"	  :	  "¤cÖ",	"র‌্য"	 :	 "i¨",	"ক্ষ্ম"	  :	  "²",
+			"ক্ক"	:	"°",	"ক্ট"	:	"±",	"ক্ত"	:	"³",
+			"ক্ব"	:	"K¡",	"স্ক্র"	  :	  "¯Œ",		"ক্র"	:	"µ",
+			"ক্ল"	:	"K¬",	"ক্ষ"	:	"¶",	"ক্স"	:	"·",
+			"গু"   :   "¸",		"গ্ধ"	:	"»",	"গ্ন"	:	"Mœ",
+			"গ্ম"	:	"M¥",	"গ্ল"	:	"M­",	"গ্রু"	 :	 "Mªy",
+			"ঙ্ক"	:	"¼",	"ঙ্ক্ষ"	  :	  "•¶",		"ঙ্খ"	:	"•L",
+			"ঙ্গ"	:	"½",	"ঙ্ঘ"	:	"•N",	"চ্ছ্ব"	  :	  "”Q¡",
+			"চ্চ"	:	"”P",	"চ্ছ"	:	"”Q",	"চ্ঞ"	:	"”T",
+			"জ্জ্ব"	  :	  "¾¡",		"জ্জ"	:	"¾",	"জ্ঝ"	:	"À",
+			"জ্ঞ"	:	"Á",	"জ্ব"	:	"R¡",	"ঞ্চ"	:	"Â",
+			"ঞ্ছ"	:	"Ã",	"ঞ্জ"	:	"Ä",	"ঞ্ঝ"	:	"Å",
+			"ট্ট"	:	"Æ",	"ট্ব"	:	"U¡",	"ট্ম"	:	"U¥",
+			"ড্ড"	:	"Ç",	"ণ্ট"	:	"È",	"ণ্ঠ"	:	"É",
+			"ন্স"	:	"Ý",	"ণ্ড"	:	"Ê",	"ন্তু"	 :	 "š‘",
+			"ণ্ব"	:	"Y^",	"ত্ত্ব"	  :	  "Ë¡",		"ত্ত"	:	"Ë",
+			"ত্থ"	:	"Ì",	"ত্ন"	:	"Zœ",	"ত্ম"	:	"Z¥",
+			"ন্ত্ব"	  :	  "š—¡",	"ত্ব"	:	"Z¡",	"থ্ব"	:	"_¡",
+			"দ্গ"	:	"˜M",	"দ্ঘ"	:	"˜N",	"দ্দ"	:	"Ï",
+			"দ্ধ"	:	"×",	"দ্ব"	:	"˜¡",	"দ্ব"	:	"Ø",
+			"দ্ভ"	:	"™¢",	"দ্ম"	:	"Ù",	"দ্রু"	 :	 "`ª“",
+			"ধ্ব"	:	"aŸ",	"ধ্ম"	:	"a¥",	"ন্ট"	:	"›U",
+			"ন্ঠ"	:	"Ú",	"ন্ড"	:	"Û",	"ন্ত্র"	  :	  "š¿",
+			"ন্ত"	:	"š—",	"স্ত্র"	  :	  "¯¿",		"ত্র"	:	"Î",
+			"ন্থ"	:	"š’",	"ন্দ"	:	"›`",	"ন্দ্ব"	  :	  "›Ø",
+			"ন্ধ"	:	"Ü",	"ন্ন"	:	"bœ",	"ন্ব"	:	"š^",
+			"ন্ম"	:	"b¥",	"প্ট"	:	"Þ",	"প্ত"	:	"ß",
+			"প্ন"	:	"cœ",	"প্প"	:	"à",	"প্ল"	:	"cø",
+			"প্স"	:	"á",	"ফ্ল"	:	"d¬",	"ব্জ"	:	"â",
+			"ব্দ"	:	"ã",	"ব্ধ"	:	"ä",	"ব্ব"	:	"eŸ",
+			"ব্ল"	:	"e­",	"ভ্র"	:	"å",	"ম্ন"	:	"gœ",
+			"ম্প"	:	"¤ú",	"ম্ফ"	:	"ç",	"ম্ব"	:	"¤^",
+			"ম্ভ"	:	"¤¢",	"ম্ভ্র"	  :	  "¤£",		"ম্ম"	:	"¤§",
+			"ম্ল"	:	"¤­",	"্র"   :   "ª",		"রু"   :   "i“",
+			"রূ"   :   "iƒ",	"ল্ক"	:	"é",	"ল্গ"	:	"ê",
+			"ল্ট"	:	"ë",	"ল্ড"	:	"ì",	"ল্প"	:	"í",
+			"ল্ফ"	:	"î",	"ল্ব"	:	"j¦",	"ল্ম"	:	"j¥",
+			"ল্ল"	:	"jø",	"শু"   :   "ï",		"শ্চ"	:	"ð",
+			"শ্ন"	:	"kœ",	"শ্ব"	:	"k¦",	"শ্ম"	:	"k¥",
+			"শ্ল"	:	"k­",	"ষ্ক"	:	"®‹",	"ষ্ক্র"	  :	  "®Œ",
+			"ষ্ট"	:	"ó",	"ষ্ঠ"	:	"ô",	"ষ্ণ"	:	"ò",
+			"ষ্প"	:	"®ú",	"ষ্ফ"	:	"õ",	"ষ্ম"	:	"®§",
+			"স্ক"	:	"¯‹",	"স্ট"	:	"÷",	"স্খ"	:	"ö",
+			"স্ত"	:	"¯Í",	"স্তু"	 :	 "¯‘",	"স্থ"	:	"¯’",
+			"স্ন"	:	"mœ",	"স্প"	:	"¯ú",	"স্ফ"	:	"ù",
+			"স্ব"	:	"¯^",	"স্ম"	:	"¯§",	"স্ল"	:	"¯­",
+			"হু"   :   "û",		"হ্ণ"	:	"nè",	"হ্ব"	:	"nŸ",
+			"হ্ন"	:	"ý",	"হ্ম"	:	"þ",	"হ্ল"	:	"n¬",
+			"হৃ"   :   "ü",		"র্"   :   "©",		"্র"   :   "«",
+			"্য"   :   "¨",		"্"	  :	  "&",	"আ"	  :	  "Av",
+			"অ"	  :	  "A",	"ই"	  :	  "B",	"ঈ"	  :	  "C",
+			"উ"	  :	  "D",	"ঊ"	  :	  "E",	"ঋ"	  :	  "F",
+			"এ"	  :	  "G",	"ঐ"	  :	  "H",	"ও"	  :	  "I",
+			"ঔ"	  :	  "J",	"ক"	  :	  "K",	"খ"	  :	  "L",
+			"গ"	  :	  "M",	"ঘ"	  :	  "N",	"ঙ"	  :	  "O",
+			"চ"	  :	  "P",	"ছ"	  :	  "Q",	"জ"	  :	  "R",
+			"ঝ"	  :	  "S",	"ঞ"	  :	  "T",	"ট"	  :	  "U",
+			"ঠ"	  :	  "V",	"ড"	  :	  "W",	"ঢ"	  :	  "X",
+			"ণ"	  :	  "Y",	"ত"	  :	  "Z",	"থ"	  :	  "_",
+			"দ"	  :	  "`",	"ধ"	  :	  "a",	"ন"	  :	  "b",
+			"প"	  :	  "c",	"ফ"	  :	  "d",	"ব"	  :	  "e",
+			"ভ"	  :	  "f",	"ম"	  :	  "g",	"য"	  :	  "h",
+			"র"	  :	  "i",	"ল"	  :	  "j",	"শ"	  :	  "k",
+			"ষ"	  :	  "l",	"স"	  :	  "m",	"হ"	  :	  "n",
+			"ড়"	  :	  "o",	"ঢ়"	  :	  "p",	"য়"	  :	  "q",
+			"ৎ"	  :	  "r",	"০"	  :	  "0",	"১"	  :	  "1",
+			"২"	  :	  "2",	"৩"	  :	  "3",	"৪"	  :	  "4",
+			"৫"	  :	  "5",	"৬"	  :	  "6",	"৭"	  :	  "7",
+			"৮"	  :	  "8",	"৯"	  :	  "9",	"া"	  :	  "v",
+			"ি"	  :	  "w",	"ী"	  :	  "x",	"ু"	  :	  "y",
+			"ূ"	  :	  "~",	"ৃ"	  :	  "…",	"ে"	  :	  "‡",
+			"ৈ"	  :	  "‰",	"ৗ"	  :	  "Š",	"ং"	  :	  "s",
 			"ঃ"	  :	  "t",	"ঁ"	  :	  "u"
 		}
 
 		pattern = 'ো'
 		replacement = 'ো'
 		srcString = util.preg_replace(pattern, replacement, srcString)
-	
+
 		pattern = 'ৌ'
 		replacement = 'ৌ'
+		srcString = util.preg_replace(pattern, replacement, srcString)
+
+		pattern ='ড়'
+		replacement = "ড়"
+		srcString = util.preg_replace(pattern, replacement, srcString)
+
+		pattern ='য়'
+		replacement = "য়"
 		srcString = util.preg_replace(pattern, replacement, srcString)
 
 		# make correction
@@ -572,15 +580,15 @@ class Unicode:
 
 		#inv_conversionMap = {v: k for k, v in conversionMap.items()}
 		srcString = util.doCharMap(srcString, main_char)
-		
+
 		if srcString[-1:] == '©':
 			srcString = srcString[:-1]
-		
+
 		srcString = srcString.replace("q‡", "‡q")
 		srcString = srcString.replace("o‡", "‡o")
-		
+
 		#pos-fix ------------------
-		
+
 		srcString = list(srcString)
 
 
@@ -595,7 +603,7 @@ class Unicode:
 				i+=2
 			else:
 				i+=1
-				
+
 		srcString = "".join(srcString)
 		srcString = srcString[2:]
 		return srcString
